075065c9b970c839c5e8232159cda265494262b5
diegoanro22 <82407480+diegoanro22@users.noreply.github.com>
2025-06-05 04:25:51 -0600

    update: import statement and adjust CSS class in GuitarSpline component

--- app/components/GuitarSpline.tsx
+++ app/components/GuitarSpline.tsx
@@ -1,6 +1,6 @@
 // src/app/components/GuitarSpline.tsx
 'use client';
-import React, {  } from 'react';
+import React from 'react';
 import Spline from '@splinetool/react-spline';
 import { useAudio } from '@/app/contexts/AudioContext';
 
@@ -9,7 +9,7 @@ interface GuitarSplineProps {
 }
 
 const GuitarSpline: React.FC<GuitarSplineProps> = ({ onGuitarClick }) => {
-  const {audioRef} = useAudio();
+  const { audioRef } = useAudio();
   const splineSceneUrl = 'https://prod.spline.design/6j-Q7nxkmFqoYOS9/scene.splinecode';
 
   const handleClick = () => {
@@ -25,7 +25,13 @@ const GuitarSpline: React.FC<GuitarSplineProps> = ({ onGuitarClick }) => {
   };
 
   return (
-    <div className="relative w-full h-0 pb-[56.25%]">
+    <div className="relative w-full h-0 pb-[100%] md:pb-[56.25%]">
+      {/* 
+        - pb-[75%] aplicará 75% de padding-bottom (ratio 4:3 aproximadamente)
+          en pantallas pequeñas (por defecto).
+        - md:pb-[56.25%] anula ese padding-bottom a partir de pantallas >= 768px,
+          volviendo a ratio 16:9.
+      */}
       <div className="absolute inset-0">
         <Spline scene={splineSceneUrl} onMouseDown={handleClick} />
       </div>
